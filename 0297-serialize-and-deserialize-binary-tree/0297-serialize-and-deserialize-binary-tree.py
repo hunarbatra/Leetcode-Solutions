2cfd9da15c6b97fa02987199708e6c27a1fb26cc
Hunar Batra <35395835+hunarbatra@users.noreply.github.com>
2025-03-27 03:03:04 +0530

    Time: 73 ms (97.59%), Space: 21.4 MB (39.47%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -7,7 +7,7 @@
 
 class Codec:
 
-    def serialize(self, root):
+    def serialize(self, root): # iterative
         """Encodes a tree to a single string.
         
         :type root: TreeNode
@@ -30,7 +30,7 @@ class Codec:
 
         return ",".join(res)
 
-    def deserialize(self, data):
+    def deserialize(self, data): # iterative
         """Decodes your encoded data to tree.
         
         :type data: str
@@ -58,6 +58,39 @@ class Codec:
             index += 1
 
         return root
+
+    def serialize(self, root): # recursive
+        res = []
+
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ",".join(res)
+
+    def deserialize(self, data: str) -> Optional[TreeNode]: # recursive
+        vals = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+
+            return node
+
+        return dfs()
         
 
 # Your Codec object will be instantiated and called as such:
