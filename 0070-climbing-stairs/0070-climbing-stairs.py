3ab75c3ad062c96cf1fa61ff1f3e6768524fc1d4
Hunar Batra <35395835+hunarbatra@users.noreply.github.com>
2026-02-12 10:13:05 +0530

    Time: 0 ms (100%), Space: 19.3 MB (56.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        lookup = [0] * (n+1)
-        lookup[0] = 1
-        lookup[1] = 1
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
 
         for i in range(2, n+1):
-            lookup[i] = lookup[i-1] + lookup[i-2]
+            dp[i] = dp[i-1] + dp[i-2]
 
-        return lookup[-1]
+        return dp[-1]
