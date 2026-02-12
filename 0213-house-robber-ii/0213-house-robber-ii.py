b542eb36e498cfc59022cd08c818657cfe56c5f1
Hunar Batra <35395835+hunarbatra@users.noreply.github.com>
2026-02-12 11:16:22 +0530

    Time: 0 ms (100%), Space: 19.4 MB (20.32%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,15 +4,13 @@ class Solution:
             return nums[0]
         if len(nums) == 2:
             return max(nums)
-
-        def rob_linear(houses):
+            
+        def rob_val(houses):
             prev, curr = 0, 0
+
             for money in houses:
-                new_prev = curr
-                curr = max(curr, prev + money)
-                prev = new_prev 
-                # prev, curr = curr, max(curr, prev + money) # simply do this instead
+                prev, curr = curr, max(prev + money, curr)
 
             return curr
 
-        return max(rob_linear(nums[1:]), rob_linear(nums[:-1]))
+        return max(rob_val(nums[1:]), rob_val(nums[:-1]))
